5173c95da866c409e3d1d646b35887074453af62
24digital <24digital@gmail.com>
2014-06-18 11:32:00 -0400

    s

--- src/main/java/Controllers/Home.java
+++ src/main/java/Controllers/Home.java
@@ -1,11 +1,15 @@
 package Controllers;
 
+import logic.Expert;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Marion on 6/18/2014.
@@ -24,7 +28,15 @@ public class Home extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         String k = req.getParameter("color");
+        Expert expert = new Expert();
+        List result = expert.getList();
         PrintWriter out = resp.getWriter();
-        out.println("<html>" +"<body>"+"Hello Marion"+ k+"</body>"+"</html>");
+        Iterator it = result.iterator();
+        while(it.hasNext())
+        {
+            out.println("<html>" +"<body>"+"Hello Marion"+ " "+it.next() +"</body>"+"</html>");
+        }
+
+
     }
 }
---
+++ src/main/java/logic/Expert.java
@@ -0,0 +1,23 @@
+package logic;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Marion on 6/18/2014.
+ */
+public class Expert {
+    public Expert() {
+list.add("Heavy");
+        list.add("Small");
+    }
+    List list = new LinkedList();
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+}
